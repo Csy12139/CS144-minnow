e9e0ece8c3d9798e89fa15c2f1c4b0727240b7f0
shinnenijou <littlesword111@gmail.com>
2023-10-21 03:36:26 +0800

    Warp32 class for check2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,17 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>(n) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t high_32 = checkpoint & 0xFFFFFFFF00000000;
+  uint64_t roundpoint_1 = static_cast<uint64_t>(raw_value_ - zero_point.raw_value_) + high_32;
+  uint64_t roundpoint_2 = roundpoint_1 <= checkpoint ? roundpoint_1 + 0x100000000 : roundpoint_1 - 0x100000000;
+  uint64_t diff_1 = roundpoint_1 > checkpoint ? roundpoint_1 - checkpoint : checkpoint - roundpoint_1;
+  uint64_t diff_2 = roundpoint_2 > checkpoint ? roundpoint_2 - checkpoint : checkpoint - roundpoint_2;
+  uint64_t ret = diff_1 < diff_2 ? roundpoint_1 : roundpoint_2;
+
+  return ret;
 }
