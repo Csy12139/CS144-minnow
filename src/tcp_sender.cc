029b10b4edda3318615a9a0c324206c5e5336c12
shinnenijou <littlesword111@gmail.com>
2023-10-25 21:51:30 +0800

    initial commit of sender class for check3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -5,49 +5,189 @@
 
 using namespace std;
 
+void RetransmissionTimer::restart( uint64_t ms_timeout )
+{
+  m_timer = 0;
+  m_timeout = ms_timeout;
+  m_running = true;
+}
+
+void RetransmissionTimer::elapse( uint64_t ms_time )
+{
+  m_timer = m_running ? m_timer + ms_time : m_timer;
+}
+
+void RetransmissionTimer::stop()
+{
+  m_running = false;
+  m_timeout = UINT64_MAX;
+  m_timer = 0; 
+}
+
+RetransmissionTimeout::RetransmissionTimeout( uint64_t initial_RTO_ms )
+  : m_value( initial_RTO_ms ), m_init_value( initial_RTO_ms )
+{}
+
+void RetransmissionTimeout::set_timeout( RestransmissionEvent event )
+{
+  switch ( event ) {
+    case TIMEOUT:
+      m_value += m_value;
+      break;
+
+    case SUCCESSFUL_RECEIPT:
+      m_value = m_init_value;
+      break;
+
+    default:
+      break;
+  }
+}
+
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
+  : m_isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), m_RTO_ms_( initial_RTO_ms )
 {}
+
+uint64_t TCPSender::get_absolute_seqno() const
+{
+  return m_outstanding_messages_.empty()
+           ? m_window_left
+           : m_outstanding_messages_.rbegin()->first + m_outstanding_messages_.rbegin()->second.sequence_length();
+}
+
+void TCPSender::push_message( std::string payload, bool syn, bool fin )
+{
+  uint64_t absolute_seqno = get_absolute_seqno();
+
+  TCPSenderMessage message;
+  message.seqno = Wrap32::wrap( absolute_seqno, m_isn_ );
+  message.payload = Buffer( std::move( payload ) );
+  message.SYN = syn;
+  message.FIN = fin;
+
+  m_outstanding_messages_.insert( std::make_pair( absolute_seqno, message ) );
+  m_send_queue_.push( std::move( message ) );
+}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+  return get_absolute_seqno() - m_window_left;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return m_consecutive_retransmissions;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-  // Your code here.
-  return {};
+  if ( m_send_queue_.empty() )
+    return {};
+
+  TCPSenderMessage message = std::move( m_send_queue_.front() );
+  m_send_queue_.pop();
+
+  if ( !m_retransmission_timer_.is_running() )
+    m_retransmission_timer_.restart( m_RTO_ms_.value() );
+
+  return message;
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+  if ( !m_syc_pushed ) {
+    m_fin_pushed = outbound_stream.bytes_buffered() == 0 && outbound_stream.is_finished();
+    m_syc_pushed = true;
+    push_message( {}, m_syc_pushed, m_fin_pushed );
+  }
+
+  if ( m_fin_pushed )
+    return;
+
+  string payload;
+
+  uint64_t window_right = m_window_left == m_window_right ? m_window_right + 1 : m_window_right;
+
+  while (!m_fin_pushed && get_absolute_seqno() + TCPConfig::MAX_PAYLOAD_SIZE <= window_right) {
+    read( outbound_stream, TCPConfig::MAX_PAYLOAD_SIZE, payload );
+    bool is_fin_msg = payload.size() + get_absolute_seqno() < window_right && outbound_stream.bytes_buffered() == 0 && outbound_stream.is_finished();
+
+    if (payload.empty() && !is_fin_msg)
+      break;
+    
+    m_fin_pushed = m_fin_pushed || is_fin_msg;
+    push_message( std::move( payload ) , false, is_fin_msg);
+  }
+
+  if ( !m_fin_pushed && get_absolute_seqno() < window_right ) {
+    read( outbound_stream, window_right - get_absolute_seqno(), payload );
+    bool is_fin_msg = payload.size() + get_absolute_seqno() < window_right && outbound_stream.bytes_buffered() == 0 && outbound_stream.is_finished();
+
+    if (!payload.empty() || is_fin_msg)
+    {
+      m_fin_pushed = m_fin_pushed || is_fin_msg;
+      push_message( std::move( payload ) , false, is_fin_msg);
+    }
+  }
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
-  // Your code here.
-  return {};
+  TCPSenderMessage message;
+  message.seqno = Wrap32::wrap( get_absolute_seqno(), m_isn_ );
+  return message;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  if ( !msg.ackno.has_value() )
+    return;
+
+  uint64_t absolute_ackno = msg.ackno.value().unwrap( m_isn_, m_window_left );
+
+  bool sucessful_recipt = false;
+
+  while ( !m_outstanding_messages_.empty() ) {
+    uint64_t wait_for_ackno
+      = m_outstanding_messages_.begin()->first + m_outstanding_messages_.begin()->second.sequence_length();
+
+    if ( absolute_ackno < wait_for_ackno || absolute_ackno > get_absolute_seqno())
+      break;
+
+    // sucessful receipt
+    m_outstanding_messages_.erase( m_outstanding_messages_.begin() );
+    sucessful_recipt = true;
+  }
+
+  if ( sucessful_recipt ) {
+    m_RTO_ms_.set_timeout( RetransmissionTimeout::SUCCESSFUL_RECEIPT );
+    m_consecutive_retransmissions = 0;
+    m_window_left = absolute_ackno;
+
+    if ( !m_outstanding_messages_.empty() )
+      m_retransmission_timer_.restart( m_RTO_ms_.value() );
+    else
+      m_retransmission_timer_.stop();
+  }
+
+  m_window_right = m_window_left + msg.window_size;
 }
 
-void TCPSender::tick( const size_t ms_since_last_tick )
+void TCPSender::tick( const uint64_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  m_timer_ += ms_since_last_tick;
+  m_retransmission_timer_.elapse( ms_since_last_tick );
+
+  if ( m_retransmission_timer_.is_timeout() ) {
+    // resend the earliest outstanding message
+    m_send_queue_.push( m_outstanding_messages_.begin()->second );
+
+    if ( m_window_right - m_window_left > 0 ) {
+      m_RTO_ms_.set_timeout( RetransmissionTimeout::TIMEOUT );
+      m_consecutive_retransmissions++;
+    }
+
+    m_retransmission_timer_.restart( m_RTO_ms_.value() );
+  }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,11 +3,61 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include <map>
+#include <queue>
+#include <utility>
+
+class RetransmissionTimeout
+{
+private:
+  uint64_t m_value {};
+  uint64_t m_init_value {};
+public:
+  enum RestransmissionEvent
+  {
+    TIMEOUT,
+    SUCCESSFUL_RECEIPT,
+  };
+
+  RetransmissionTimeout(uint64_t initial_RTO_ms);
+  uint64_t value() const { return m_value; }
+  void set_timeout(RestransmissionEvent event);
+};
+
+class RetransmissionTimer
+{
+private:
+  uint64_t m_timer {};
+  uint64_t m_timeout {UINT64_MAX};
+  bool m_running {};
+public:
+  uint64_t value() const { return m_timer; }
+  void stop();
+  bool is_running() const { return m_running; }
+  bool is_timeout() const { return m_running && m_timer >= m_timeout; }
+  void restart(uint64_t ms_timeout);
+  void elapse(uint64_t ms_time);
+};
 
 class TCPSender
 {
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+private:
+  std::map<uint64_t, TCPSenderMessage> m_outstanding_messages_ {}; //seqno -> TCPSenderMessage
+  std::queue<TCPSenderMessage> m_send_queue_ {};
+
+  bool m_syc_pushed {};
+  bool m_fin_pushed {};
+
+  uint64_t m_timer_ {};
+  RetransmissionTimer m_retransmission_timer_ {};
+  Wrap32 m_isn_ { 0 };
+  RetransmissionTimeout m_RTO_ms_ {0};
+  uint64_t m_consecutive_retransmissions {};
+  uint64_t m_window_left {};
+  uint64_t m_window_right {};
+
+  uint64_t get_absolute_seqno() const;
+  void push_message( std::string payload, bool syn = false, bool fin = false );
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
@@ -26,7 +76,7 @@ public:
   void receive( const TCPReceiverMessage& msg );
 
   /* Time has passed by the given # of milliseconds since the last time the tick() method was called. */
-  void tick( uint64_t ms_since_last_tick );
+  void tick( const uint64_t ms_since_last_tick );
 
   /* Accessors for use in testing */
   uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
